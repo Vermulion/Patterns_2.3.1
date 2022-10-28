6fc2f95a3e66eead9fbb8d7f235481c995017ecc
Viacheslav Ryabov <vermulion@gmail.com>
2022-10-28 22:20:54 +0300

    Tests added

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -15,15 +15,22 @@ public class DataGenerator {
 
     @UtilityClass
     public static class Registration {
+
         public static DeliveryInfo generateInfo(int days) {
 
             Faker faker = new Faker(new Locale("ru"));
             return new DeliveryInfo(
-                    faker.address().cityName(),
+                    generateCity(),
                     LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
                     faker.name().firstName() + " " + faker.name().lastName(),
                     faker.phoneNumber().phoneNumber()
             );
         }
+    }
+
+    String generateCity() {
+        String[] cities = new String[]{"Пермь", "Владимир", "Вологда", "Нижний Новгород", "Краснодар", "Санкт-Петербург"};
+        int itemIndex = (int) (Math.random() * cities.length);
+        return cities[itemIndex];
     }
 }
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.delivery.data.DataGenerator;
 import ru.netology.delivery.data.DeliveryInfo;
 
-import static com.codeborne.selenide.Selenide.*;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.delivery.data.DataGenerator.Registration.generateInfo;
 
 class DeliveryTest {
@@ -25,12 +29,59 @@ class DeliveryTest {
     void shouldSuccessfullyPlanMeetingAndChangeDate() {
         DeliveryInfo firstDate = generateInfo(3);
         DeliveryInfo secondDate = generateInfo(15);
+        $x(".//span[@data-test-id='city']//child::input[@placeholder]").val(firstDate.getCity());
+        $x(".//span[@data-test-id='date']//child::input[@placeholder]").val(firstDate.getDate());
+        $x(".//span[@data-test-id='name']//child::input").val(firstDate.getName());
+        $x(".//span[@data-test-id='phone']//child::input").val(firstDate.getPhoneNumber());
+        $x(".//label[@data-test-id='agreement']").click();
+        $x(".//button[@role='button']//child::span[contains(text(), 'Запланировать')]").click();
+        $x(".//div[@data-test-id='success-notification']/child::div[@class='notification__title']").
+                shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно!"));
+        $x(".//div[@data-test-id='success-notification']/child::div[@class='notification__content']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstDate.getDate()));
+        $x(".//span[@data-test-id='date']//child::input[@placeholder]")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x(".//span[@data-test-id='date']//child::input").val(secondDate.getDate());
+        $x(".//button[@role='button']//child::span[contains(text(), 'Запланировать')]").click();
+
+        $x(".//div[@data-test-id='replan-notification']/child::" +
+                "div[contains(text(), 'Необходимо подтверждение')]")
+                .shouldBe(visible, Duration.ofSeconds(15));
+        $x(".//span[contains(text(), 'Перепланировать')]//ancestor::button").click();
+        $x(".//div[@data-test-id='success-notification']/child::div[@class='notification__content']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondDate.getDate()));
+    }
+
+    @Test
+    @DisplayName("Should successfully plan meeting and try to repeat to send the form with the same data")
+    void shouldSuccessfullyPlanMeetingAndRepeatSameDate() {
+        DeliveryInfo firstDate = generateInfo(3);
         Configuration.holdBrowserOpen = true;
         $x(".//span[@data-test-id='city']//child::input[@placeholder]").val(firstDate.getCity());
         $x(".//span[@data-test-id='date']//child::input[@placeholder]").val(firstDate.getDate());
         $x(".//span[@data-test-id='name']//child::input").val(firstDate.getName());
         $x(".//span[@data-test-id='phone']//child::input").val(firstDate.getPhoneNumber());
         $x(".//label[@data-test-id='agreement']").click();
+        $x(".//button[@role='button']//child::span[contains(text(), 'Запланировать')]").click();
+        $x(".//div[@data-test-id='success-notification']/child::div[@class='notification__title']").
+                shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно!"));
+        $x(".//div[@data-test-id='success-notification']/child::div[@class='notification__content']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstDate.getDate()));
+        $x(".//span[@data-test-id='date']//child::input[@placeholder]")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x(".//span[@data-test-id='date']//child::input").val(firstDate.getDate());
+        $x(".//button[@role='button']//child::span[contains(text(), 'Запланировать')]").click();
 
+        $x(".//div[@data-test-id='replan-notification']/child::" +
+                "div[contains(text(), 'Необходимо подтверждение')]")
+                .shouldBe(visible, Duration.ofSeconds(15));
+        $x(".//span[contains(text(), 'Перепланировать')]//ancestor::button").click();
+        $x(".//div[@data-test-id='success-notification']/child::div[@class='notification__content']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstDate.getDate()));
     }
+
 }
